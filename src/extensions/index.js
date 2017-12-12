0ee42fc3fe9ccb731dcf2b45a1a703746dd3e3c9
decaffeinate <dinoboff@hotmail.com>
2017-12-12 00:47:28 +0000

    decaffeinate: Run post-processing cleanups on consumer.coffee and 20 other files

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,2 +1,4 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 exports.Content  = require('./content');
 exports.Outcomes = require('./outcomes');
--- src/ims-lti.js
+++ src/ims-lti.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const extensions = require('./extensions');
 
 // Export the main object
